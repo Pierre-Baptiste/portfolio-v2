12ff63a570c7401dffad39fd50ba67745afb3e40
Pierre-Baptiste Dupire <pierre-baptiste@omnidoc.fr>
2020-10-29 23:11:50 +0100

    provided messages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,16 @@ import "styles/index.css";
 import { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { IntlProvider } from "react-intl";
+import { messages } from "messages";
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
+  const { locale, defaultLocale } = router;
   return (
-    <IntlProvider locale={router.locale || router.defaultLocale}>
+    <IntlProvider
+      locale={locale || defaultLocale}
+      messages={messages[locale || defaultLocale]}
+    >
       <Component {...pageProps} />
     </IntlProvider>
   );
