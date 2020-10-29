f0a24b5f0c717faea5b10077ba502a3c81ef1ed8
Pierre-Baptiste Dupire <pierre-baptiste@omnidoc.fr>
2020-10-29 23:11:39 +0100

    added 404 page

---
+++ messages.js
@@ -0,0 +1,13 @@
+export const messages = {
+  en: {
+    "not_found.title": "Oh dear. Are you lost ?",
+    "not_found.subtitle":
+      "But dont worry, you can find plenty of other things on our homepage.",
+    "not_found.button": "back to homepage",
+  },
+  fr: {
+    "not_found.title": "Oups, voilà qui n'était pas prévu !",
+    "not_found.subtitle": "Désolé mais ce lien est introuvable",
+    "not_found.button": "Retour à la page d'accueil",
+  },
+};
