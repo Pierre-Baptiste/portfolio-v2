366ce220508abca8af48b162c4fe6c33a5576a1f
Pierre-Baptiste Dupire <pierre-baptiste@omnidoc.fr>
2020-10-29 22:20:36 +0100

    added basic i18n

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import "styles/index.css";
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { IntlProvider } from "react-intl";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  return (
+    <IntlProvider locale={router.locale || router.defaultLocale}>
+      <Component {...pageProps} />
+    </IntlProvider>
+  );
 }
 
 export default App;
