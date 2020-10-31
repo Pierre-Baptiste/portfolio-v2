17db89160ea5369c74677aa07fe68949f5eed402
Pierre-Baptiste Dupire <pierre-baptiste@omnidoc.fr>
2020-10-31 15:21:07 +0100

    improved layout

--- components/core/Layout.tsx
+++ components/core/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Layout = ({ children }) => {
-  return <div className="max-w-container m-auto">{children}</div>;
+  return <div className="max-w-container px-6 m-auto">{children}</div>;
 };
 
 export { Layout };
