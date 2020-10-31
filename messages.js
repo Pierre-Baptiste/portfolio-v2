f89fdffe3828516e9f282709bab2d9543daa3c09
Pierre-Baptiste Dupire <pierre-baptiste@omnidoc.fr>
2020-10-31 12:58:32 +0100

    app bar messages

--- messages.js
+++ messages.js
@@ -4,10 +4,20 @@ export const messages = {
     "not_found.subtitle":
       "But dont worry, you can find plenty of other things on our homepage.",
     "not_found.button": "back to homepage",
+    "app_bar.home": "Home",
+    "app_bar.works": "Works",
+    "app_bar.about": "About",
+    "app_bar.gallery": "Gallery",
+    "app_bar.contact": "Contact",
   },
   fr: {
     "not_found.title": "Oups, voilà qui n'était pas prévu !",
     "not_found.subtitle": "Désolé mais ce lien est introuvable",
     "not_found.button": "Retour à la page d'accueil",
+    "app_bar.home": "Accueil",
+    "app_bar.works": "Travaux",
+    "app_bar.about": "À propos",
+    "app_bar.gallery": "Galerie",
+    "app_bar.contact": "Contacter",
   },
 };
