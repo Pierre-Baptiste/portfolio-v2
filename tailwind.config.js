df55e5ec718001a83c4a61491b16afd8dc11ed9f
Pierre-Baptiste Dupire <pierre-baptiste@omnidoc.fr>
2020-10-31 12:58:25 +0100

    added custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   future: {},
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        action: "#001aff",
+        paper: "#F2F2F2",
+        lightgray: "#808080",
+      },
+      maxWidth: {
+        container: "97rem",
+      },
+    },
   },
   variants: {},
   plugins: [],
