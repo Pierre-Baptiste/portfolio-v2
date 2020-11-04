3921433e2f344d1492e7a6ea89a8909feaac47a1
Pierre-Baptiste Dupire <pierre-baptiste@omnidoc.fr>
2020-11-04 23:14:29 +0100

    added hero messages

--- messages.js
+++ messages.js
@@ -9,6 +9,24 @@ export const messages = {
     "app_bar.about": "About",
     "app_bar.gallery": "Gallery",
     "app_bar.contact": "Contact",
+    "landing.title.hello": "Hello",
+    "landing.title.iam": "I am",
+    "landing.title.pb": "Pierre-Baptiste",
+    "landing.title.engineer": "an engineer",
+    "landing.title.developer": "a developer",
+    "landing.title.passionate": "passionate",
+    "works.hero.title": "| 02 : Works |",
+    "about.hero.title": "| 03 : About |",
+    "gallery.hero.title": "| 04 : Gallery |",
+    "contact.hero.title": "| 05 : Contact |",
+    "works.hero.subtitle":
+      'Here is a showcase of my most relevant projects. They include some personal ones as well as some school related ones. You can learn more about them by clicking on the "Plus" button, I try to give some more details about each one and, in team projects, I focus mainly on my input.',
+    "about.hero.subtitle":
+      "Want to learn more about my career path and education? Here you will find everything you would want to know about it.",
+    "gallery.hero.subtitle":
+      "Sometimes I take surprisingly good/cool pictures, other times I have a little fun with computer art... Whatever it is, it falls here!",
+    "contact.hero.subtitle":
+      "Want to get in touch with me? You can reach me through e-mail, on the social media links and by filling out the form.",
   },
   fr: {
     "not_found.title": "Oups, voilà qui n'était pas prévu !",
@@ -19,5 +37,23 @@ export const messages = {
     "app_bar.about": "À propos",
     "app_bar.gallery": "Galerie",
     "app_bar.contact": "Contacter",
+    "landing.title.hello": "Bonjour",
+    "landing.title.iam": "Je suis",
+    "landing.title.pb": "Pierre-Baptiste",
+    "landing.title.engineer": "ingénieur",
+    "landing.title.developer": "développeur",
+    "landing.title.passionate": "passionné",
+    "works.hero.title": "| 02 : Travaux |",
+    "about.hero.title": "| 03 : À propos |",
+    "gallery.hero.title": "| 04 : Galerie |",
+    "contact.hero.title": "| 05 : Contact |",
+    "works.hero.subtitle":
+      "Voici une vitrine de mes projets les plus pertinents. Il y en a des personnels et d'autres liés à l'école. Vous pouvez en apprendre plus sur eux en cliquant sur le bouton \"Plus\", j'essaie de donner plus de détails sur chacun d'eux et, dans les projets d'équipe, je me concentre principalement sur mon apport.",
+    "about.hero.subtitle":
+      "Vous voulez en savoir plus sur mon parcours et mes études ? Vous trouverez ici tout ce que vous voudrez savoir à ce sujet.",
+    "gallery.hero.subtitle":
+      "De temps en temps je prends des photos étonnamment bonnes/cool, d'autres fois je m'amuse avec l'art digital.... Quoi que ce soit, ça tombe ici!",
+    "contact.hero.subtitle":
+      "Vous souhaitez me contacter ? Vous pouvez le faire sur mon e-mail, à travers les différents réseaux sociaux présentés ou en passant par le formulaire.",
   },
 };
